8537a0b0b87974ec65d57c14f142381b0510c7cb
fiqriashiddiqi <fiqriashiddiqi@gmail.com>
2022-10-17 07:42:24 +0700

    db

--- apps/shinta-fiqri/koneksi.php
+++ apps/shinta-fiqri/koneksi.php
@@ -1,10 +1,10 @@
 <?php
   $host = "localhost"; 
-  // $user = "phpmyadmin";
-  // $pass = "8cadb11f56";
-  $user = "root";
-  $pass = "";
-  $nama_db = "db_kiki"; //nama database
+  $user = "phpmyadmin";
+  $pass = "8cadb11f56";
+  // $user = "root";
+  // $pass = "";
+  $nama_db = "db_shinta_fiqri"; //nama database
   $koneksi = mysqli_connect($host,$user,$pass,$nama_db); //pastikan urutan nya seperti ini, jangan tertukar
 
   // if(!$koneksi){ //jika tidak terkoneksi maka akan tampil error
